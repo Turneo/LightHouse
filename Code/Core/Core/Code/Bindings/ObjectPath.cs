61307999327f77a935acf5a9679fc28beb96921b
Klaus Stefan Gerber <stefan.gerber@turneo.com>
2015-06-11 22:27:22 +0200

    Update comments in the ObjectPath.

--- Code/Core/Core/Code/Bindings/ObjectPath.cs
+++ Code/Core/Core/Code/Bindings/ObjectPath.cs
@@ -9,12 +9,12 @@ namespace LightHouse.Core.Bindings
     public class ObjectPath
     {        
         /// <summary>
-        /// REVIEW: Source IObject where the Path can be found
+        /// Source of the binding as IObject.
         /// </summary>
         public IObject Source { get; set; }
 
         /// <summary>
-        /// REVIEW: Path where the Value can be found in the Source
+        /// Path where the value can be found in the Source.
         /// </summary>
         public String Path { get; set; }
     }
